556c1a6d0689564d22842322384ee79182b24e62
EscapeGIFT <noescape892@gmail.com>
2025-08-05 02:09:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Bot, Re
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ContextTypes, PreCheckoutQueryHandler
 from telegram.error import TelegramError
 BOT_TOKEN = ('8398858310:AAFPis_fFfWNi4h4Iqc0HVl3trzMONtCD8c')
+print(f"Loaded BOT_TOKEN: {BOT_TOKEN}")
 # Load environment variables
 load_dotenv()
 
